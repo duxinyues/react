1fe0cc3715743919f669f685c0cc9c0d7bc63df3
weiyongyuan94 <yongyuan253015@gmail.com>
2019-01-18 00:24:00 +0800

    修改布局和样式

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,11 @@ export default class AboutMe extends Component {
     render() {
         return (
             <Card className="about_me">
-            <h2>关于我</h2>
+            <h2>读心</h2>
             <ul>
                 <div className="head_img"><img src={headImg} alt="" /></div>
                 <p><b>WEB前端工程师</b></p>
-                <p><b>读心</b>  ，读书，读自己！</p>
+                <p>读书，读自己！</p>
             </ul>
         </Card>
         )

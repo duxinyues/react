bf8733e22b58acdb460b1a2a5f4c63943210c184
weiyongyuan94 <yongyuan253015@gmail.com>
2019-02-27 23:44:18 +0800

    add link

--- src/components/Link.js
+++ src/components/Link.js
@@ -10,7 +10,8 @@ class FLink extends Component {
                                 <li><a href="https://github.com/weiyongyuan94" rel="opener">Github</a></li>
                                 <li><a href="https://blog.csdn.net/xuelian3015" rel="opener">CSDN</a></li>
                                 <li><a href="https://www.yuque.com/duxin-gyxgn" rel="opener">语雀</a></li>
-                                 <li><a href="https://www.jianshu.com/u/0cd38e1615f3" rel="opener">简书</a></li>
+                                <li><a href="https://www.jianshu.com/u/0cd38e1615f3" rel="opener">简书</a></li>
+                                <li><a href="http://www.zsxcool.com/" rel="opener">张思翔个人博客</a></li>
                             </ul>
                         </Card>
         )

d7a8fbdb5a64bebbaf827509b26f5c8422824423
weiyongyuan <yongyuan253015@gmail.com>
2019-02-28 10:53:31 +0800

    修改样式

--- src/components/Link.js
+++ src/components/Link.js
@@ -11,7 +11,7 @@ class FLink extends Component {
                                 <li><a href="https://blog.csdn.net/xuelian3015" rel="opener">CSDN</a></li>
                                 <li><a href="https://www.yuque.com/duxin-gyxgn" rel="opener">语雀</a></li>
                                 <li><a href="https://www.jianshu.com/u/0cd38e1615f3" rel="opener">简书</a></li>
-                                <li><a href="http://www.zsxcool.com/" rel="opener">张思翔个人博客</a></li>
+                    <li><a href="http://www.zsxcool.com/" target="_blank" rel='noreferrer noopener'>张思翔个人博客</a></li>
                             </ul>
                         </Card>
         )

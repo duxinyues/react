b2e62333c625bb71d5d3c21dd2017d5e04243ca1
duxinyues <yongyuan253015@gmail.com>
2021-07-21 23:41:43 +0800

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
 /*
  * @Author: your name
  * @Date: 2021-07-10 15:03:42
- * @LastEditTime: 2021-07-11 10:38:13
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-07-21 23:09:40
+ * @LastEditors: yongyuan at <yongyuan253015@gmail.com>
  * @Description: In User Settings Edit
- * @FilePath: \hook\src\App.js
+ * @FilePath: \react\src\App.js
  */
-import { useState, useEffect } from "react"
-import Child from "./child"
+import Child from "./components/child"
 function App() {
-  const [data, setData] = useState(0);
-  const [showChild, setShowChild] = useState(true);
-  useEffect(() => {
-    const todoList = document.getElementById("todoList");
-    const newItem = document.createElement("li");
-    newItem.innerHTML = `这是第${data}个TODO`;
-    todoList.append(newItem)
-  })
-  const add = () => {
-    setData(data + 1);
-    setShowChild(false)
-  }
-  return <>
-    <button onClick={add}>add</button>
-    <p>当前共`${data}`个TODO Item</p>
-    <ul id="todoList"></ul>
 
+  return <>
     <div>
-      {showChild && <Child />}
+      <Child />
     </div>
   </>
 }
--- src/components/child.jsx
+++ src/components/child.jsx
@@ -5,12 +5,21 @@ export default function Child() {
         console.log("挂载阶段")
         setInterval(() => {
             setCount(count + 1)
-        }, 1000)
+        }, 50000)
         return ()=>{
+            setCount(0)
             console.log("卸载阶段")
         }
+    },[count])
+    const [name,setName] = useState("duxin");
+    useEffect(()=>{
+       setTimeout(() => {
+        setName("读心")
+       }, 4000); 
     })
     return <div>
         这是子组件{count}
+
+        <p>姓名：{name}</p>
     </div>
 }

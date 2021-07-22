0f08f0423eb2f678e143731d6be14c6d68af04c4
duxinyues <yongyuan253015@gmail.com>
2021-07-22 22:28:35 +0800

    useRedcuer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 /*
  * @Author: your name
  * @Date: 2021-07-10 15:03:42
- * @LastEditTime: 2021-07-21 23:09:40
+ * @LastEditTime: 2021-07-21 23:52:26
  * @LastEditors: yongyuan at <yongyuan253015@gmail.com>
  * @Description: In User Settings Edit
  * @FilePath: \react\src\App.js
  */
-import Child from "./components/child"
+import Counter from "./components/useReducer"
 function App() {
 
   return <>
     <div>
-      <Child />
+      <Counter />
     </div>
   </>
 }

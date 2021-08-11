bfb2b91fca574d90616e58f9cae68e313ae14c2a
duxinyues <yongyuan253015@gmail.com>
2021-08-11 23:41:29 +0800

    设置redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 /*
  * @Author: your name
  * @Date: 2021-07-10 15:03:42
- * @LastEditTime: 2021-08-09 22:41:11
+ * @LastEditTime: 2021-08-09 23:50:48
  * @LastEditors: yongyuan at <yongyuan253015@gmail.com>
  * @Description: In User Settings Edit
  * @FilePath: \react\src\App.js
  */
 import React from "react";
-import InputDome  from "./components/input"
+import InputDome from "./components/input";
+import EchratsStack from "./components/echarts_stack"
+import "./App.css";
 function App() {
-  return <InputDome />
+  return <div className="container">
+    <div>
+      <InputDome />
+    </div>
+    <div>
+      <EchratsStack />
+    </div>
+  </div>
 }
 
 export default App;
---
+++ src/store.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: yongyuan253015@gmail.com
+ * @Date: 2021-08-11 23:20:54
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-08-11 23:25:04
+ * @Description: store文件
+ */

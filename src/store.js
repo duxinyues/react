db369034621d6ffdb7522c3e0e9a7f533fe2cef4
读心悦 <dxdexin@outlook.com>
2021-08-26 14:15:18 +0800

    设置store文件

--- src/store.js
+++ src/store.js
@@ -5,3 +5,21 @@
  * @LastEditTime: 2021-08-11 23:25:04
  * @Description: store文件
  */
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import allReducers from "@/reducer/reducer";
+
+const reducers = combineReducers({
+   
+})
+
+
+const initialState = {};
+const middleware = [thunk];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducers, initialState, composeEnhancers(
+    applyMiddleware(...middleware)
+))
+
+
+export default store

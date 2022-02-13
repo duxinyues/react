5010441ece07045c73f62d6966c5e265d0e6e1ed
读心悦 <1638877065@qq.com>
2022-02-13 23:15:36 +0800

    :heavy_plus_sign:  feat：useMemo

--- src/components/useMemo.jsx
+++ src/components/useMemo.jsx
@@ -3,16 +3,39 @@ import React, { useState, useMemo } from "react"
 export function UseMemoComponent() {
     const [data, setData] = useState(0);
     const [step, setStep] = useState(0)
-    const add = () => {
+    const changeData = () => {
         let newData = data + 1
         setData(newData);
+
+    }
+    const changeStep = () => {
+        let newData = step + 1;
         setStep(newData)
     }
-    const renderDatat = useMemo(() => {
-        return <span>{data}</span>
-    }, [step])
+
+    const bComponent = (data) => {
+        console.log("组件B")
+        return <span>B组件:{data}</span>
+    }
+    const renderB = useMemo(() => bComponent(data), [data])
+
+    const cComponent = (data) => {
+        console.log("组件C")
+        return <span>C组件:{data}</span>
+    }
+    const renderC = useMemo(() => cComponent(step), [step])
+    const A = () => {
+        console.log("组件A")
+        return <div>
+            {renderB}
+            <br />
+            {renderC}
+        </div>
+    }
     return <div>
-        <button onClick={add}>click</button>
-        {renderDatat}
+
+        <button onClick={changeData}>changeData</button> <button onClick={changeStep}>changeStep</button>
+        <br />
+        <A />
     </div>
 }

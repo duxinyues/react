e50a3a4811bac513b20ca2e68d95e8498d1a73fb
读心悦 <1638877065@qq.com>
2022-03-03 00:00:11 +0800

    useCallback

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import App from "./components/router"
+import App from "./components/useCallback"
 import store from "./store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";

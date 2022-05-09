6ce6e4536f1f31c0ea328e34a0272a12331f4ae3
读心悦 <1638877065@qq.com>
2022-05-09 23:23:39 +0800

    自定义hooks，避免闭包陷阱

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,8 +1,30 @@
-import React, { useState, } from "react";
+import { useState, useEffect, useRef, useLayoutEffect } from "react";
 import Modal from "./Modal"
 import "./index.scss";
+
+function useInterval(callback, time) {
+  const ref = useRef(callback)
+  useLayoutEffect(() => {
+    ref.current = callback
+  })
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      ref.current()
+    }, time)
+
+    return () => clearInterval(timer)
+  }, [])
+}
 function ModalDome() {
   const [visible, setVisible] = useState(false);
+  const [count, setCount] = useState(0);
+  useInterval(() => {
+    setCount(count => count + 1)
+  }, 1000)
+  useInterval(() => {
+    // console.log(count)
+  }, 1000)
   return <div className="modal-dome">
     <span onClick={() => { setVisible(true) }}>modal</span>
 
@@ -14,7 +36,7 @@ function ModalDome() {
         setVisible(false)
         console.log("取消")
       }}
-      onOk={()=>{
+      onOk={() => {
         alert("点击确认按钮")
       }}
     >

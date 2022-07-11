2e7d101e6e14ad1232361f397f0939cfbdaf47a4
duxinyues <yongyuan253015@gmail.com>
2022-07-11 23:11:28 +0800

    20220711

--- src/components/Three/Box.tsx
+++ src/components/Three/Box.tsx
@@ -2,7 +2,7 @@
  * @Author: duxinyues yongyuan253015@gmail.com
  * @Date: 2022-07-10 23:31:43
  * @LastEditors: duxinyues yongyuan253015@gmail.com
- * @LastEditTime: 2022-07-11 00:01:17
+ * @LastEditTime: 2022-07-11 22:11:24
  * @FilePath: \react\src\components\Three\Box.tsx
  * @Description: 
  * Copyright (c) 2022 by duxinyues email: yongyuan253015@gmail.com, All Rights Reserved.
@@ -20,19 +20,20 @@ export default function Box() {
         // 自定义几何
         const geometry = new THREE.BufferGeometry(); // 创建一个Buffer类型几何体对象
         const vertices = new Float32Array([
-            0, 0, 0, // 顶点1
-            50, 0, 0, // 顶点2
-            0, 100, 0, // 顶点3
-            0, 0, 10,// 顶点4
-            0, 0, 100,
-            50, 0, 10,
+            0, 0, 0, //顶点1坐标
+            50, 50, 50, //顶点2坐标
+            100, 100, 100, //顶点3坐标
+            200, 200, 100, //顶点4坐标
+            60, 90, 100, //顶点5坐标
+            50, 300, 10, //顶点6坐标
         ]);
         const attribue = new THREE.BufferAttribute(vertices, 2);
         geometry.attributes.position = attribue;
 
+        // 面模型
         const material0 = new THREE.MeshBasicMaterial({
             color: 0x0000ff,
-            side: THREE.DoubleSide
+            // side: THREE.DoubleSide,// 两面可见
         });
         const mesh = new THREE.Mesh(geometry, material0);
         scene.add(mesh);
@@ -40,12 +41,18 @@ export default function Box() {
         // 点模型
         const material = new THREE.PointsMaterial({
             color: "#fff",
-            size: 20
+            size: 5
         });
 
-        const points = new THREE.Points(geometry,material);
+        const points = new THREE.Points(geometry, material);
         scene.add(points);
 
+        // 线模型
+        const lineMaterial = new THREE.LineBasicMaterial({
+            color: "pink"
+        });
+        const line = new THREE.Line(geometry, lineMaterial);
+        scene.add(line)
 
         const width = window.innerWidth;
         const height = window.innerHeight;
